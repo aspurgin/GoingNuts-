88fcf4364a20adb2cee2574b6a9d918d31e19e42
aspurgin <adam.spurgin+476@gmail.com>
2014-04-22 12:41:55 -0700

    added logging functionality to every file

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ SOURCES=$(shell find . -name "*.cpp")
 OBJECTS=$(SOURCES:%.cpp=%.o)
 TARGET=foo
 LIBS= -lGL -lGLU -lGLEW -lglfw3 -lX11 -lXrandr
+CPPFLAGS+= -include "`pwd`/src/helperFiles/Util.hpp"
 
 .PHONY: all
 all: $(TARGET)
---
+++ src/helperFiles/Util.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+
+#define LOG_ERROR 0 // Bad stuff happened
+#define LOG_WARN  1 // Non-ideal stuff happened
+#define LOG_INFO  2 // Interesting stuff happened
+#define LOG_DEBUG 3 // To be used where needed
+#define LOG_TRACE 4 // Very high-volume items
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL 1
+#endif
+
+#define LOG(x, y) std::cerr << "[" << x << "] " << __FILE__ << ":" << __LINE__ << " - " << y << "\n"
+
+#if LOG_LEVEL >= LOG_ERROR
+	#define ERROR(x) LOG("ERROR", x);
+#else
+	#define ERROR(x) ((void)0)
+#endif
+
+#if LOG_LEVEL >= LOG_WARN
+	#define WARN(x) LOG("WARN", x);
+#else
+	#define WARN(x) ((void)0)
+#endif
+
+#if LOG_LEVEL >= LOG_INFO
+	#define INFO(x) LOG("INFO", x);
+#else
+	#define INFO(x) ((void)0)
+#endif
+
+#if LOG_LEVEL >= LOG_DEBUG
+	#define DEBUG(x) LOG("DEBUG", x);
+#else
+	#define DEBUG(x) ((void)0)
+#endif
+
+#if LOG_LEVEL >= LOG_TRACE
+	#define TRACE(x) LOG("TRACE", x);
+#else
+	#define TRACE(x) ((void)0)
+#endif
