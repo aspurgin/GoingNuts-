a033a36666965940cc552240244b13dd11075bec
stick <stick@crunchbang.(none)>
2014-06-04 16:25:39 -0700

    shaders

---
+++ assets/shaders/BloomShaderBlurHor_Vert.glsl
@@ -0,0 +1,18 @@
+attribute vec3 aPosition;
+
+uniform mat4 uProjMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uModelMatrix;
+
+varying vec4 bloomCoord;
+
+void main() {
+   vec4 vPosition;
+
+   vPosition = uModelMatrix * vec4(aPosition.x, aPosition.y, aPosition.z, 1);
+
+   vPosition = uViewMatrix * vPosition;
+   bloomCoord = uProjMatrix * vPosition;
+
+   gl_Position = bloomCoord;
+}
